24cf6dc7143cc700cb9dcad5a27e936ef5530cf0
AshleyMS08 <ashley.silva@sptech.school>
2026-05-12 21:58:12 -0300

    Atualizações no CSS e insert no banco

--- src/database/script-tabelas.sql
+++ src/database/script-tabelas.sql
@@ -1,62 +1,59 @@
--- Arquivo de apoio, caso você queira criar tabelas como as aqui criadas para a API funcionar.
--- Você precisa executar os comandos no banco de dados para criar as tabelas,
--- ter este arquivo aqui não significa que a tabela em seu BD estará como abaixo!
+CREATE DATABASE fermentando_afeto;
+USE fermentando_afeto;
 
-/*
-comandos para mysql server
-*/
+CREATE TABLE usuario(
+  idUsuario INT PRIMARY KEY AUTO_INCREMENT,
+  nome VARCHAR(45) NOT NULL,
+  email VARCHAR(45) UNIQUE NOT NULL,
+	CONSTRAINT chkEmail CHECK (email LIKE '%@%'),
+  senha VARCHAR(100) NOT NULL,
+  dtCriacao TIMESTAMP DEFAULT CURRENT_TIMESTAMP 
+  );
 
-CREATE DATABASE aquatech;
+CREATE TABLE categoria(
+  idCategoria INT PRIMARY KEY AUTO_INCREMENT,
+  categoria VARCHAR(45),
+	CONSTRAINT chkCategoria CHECK (categoria IN('Doce', 'Salgado',
+    'Integral', 'Sem glúten')),
+  tecnica VARCHAR(45),
+	CONSTRAINT chkTecnica CHECK (tecnica IN('Fermentação natural', 'Fermentação biológica',
+    'Massa enriquecida'))
+  );
 
-USE aquatech;
+CREATE TABLE receita(
+  idReceita INT PRIMARY KEY AUTO_INCREMENT,
+  nome VARCHAR(45),
+  ingredientes VARCHAR(1000),
+  modoPreparo TEXT,
+  nivelReceita VARCHAR(20),
+	CONSTRAINT chkNivelReceita CHECK (nivelReceita IN('Básico', 'Intermediário', 'Avançado')),
+  fkUsuario INT,
+	CONSTRAINT fkUsuarioReceita FOREIGN KEY (fkUsuario)
+	REFERENCES usuario(idUsuario),
+  fkCategoria INT,
+	CONSTRAINT fkCategoriaReceita FOREIGN KEY (fkCategoria)
+	REFERENCES categoria(idCategoria)
+  );
 
-CREATE TABLE empresa (
-	id INT PRIMARY KEY AUTO_INCREMENT,
-	razao_social VARCHAR(50),
-	cnpj CHAR(14),
-	codigo_ativacao VARCHAR(50)
-);
+CREATE TABLE comentario(
+  idComentario INT PRIMARY KEY AUTO_INCREMENT,
+  comentario VARCHAR(250),
+  fkReceita INT,
+	CONSTRAINT fkReceitaComentario FOREIGN KEY (fkReceita)
+    REFERENCES receita(idReceita),
+  fkComentarioPai INT,
+    CONSTRAINT fkComentarioPai FOREIGN KEY (fkComentarioPai)
+    REFERENCES comentario(idComentario)
+  );
 
-CREATE TABLE usuario (
-	id INT PRIMARY KEY AUTO_INCREMENT,
-	nome VARCHAR(50),
-	email VARCHAR(50),
-	senha VARCHAR(50),
-	fk_empresa INT,
-	FOREIGN KEY (fk_empresa) REFERENCES empresa(id)
-);
+CREATE TABLE preferencia(
+  idPreferencia INT PRIMARY KEY AUTO_INCREMENT,
+  nivelExperiencia VARCHAR(15),
+	CONSTRAINT chkNivelPreferencia CHECK (nivelExperiencia IN ('iniciante', 
+	'intermediário', 'profissional')),
+  objetivo VARCHAR(60),
+  fkUsuario INT,
+	CONSTRAINT fkUsuarioPreferencia FOREIGN KEY (fkUsuario)
+    REFERENCES usuario(idUsuario)
+  );
 
-CREATE TABLE aviso (
-	id INT PRIMARY KEY AUTO_INCREMENT,
-	titulo VARCHAR(100),
-	descricao VARCHAR(150),
-	fk_usuario INT,
-	FOREIGN KEY (fk_usuario) REFERENCES usuario(id)
-);
-
-create table aquario (
-/* em nossa regra de negócio, um aquario tem apenas um sensor */
-	id INT PRIMARY KEY AUTO_INCREMENT,
-	descricao VARCHAR(300),
-	fk_empresa INT,
-	FOREIGN KEY (fk_empresa) REFERENCES empresa(id)
-);
-
-/* esta tabela deve estar de acordo com o que está em INSERT de sua API do arduino - dat-acqu-ino */
-
-create table medida (
-	id INT PRIMARY KEY AUTO_INCREMENT,
-	dht11_umidade DECIMAL,
-	dht11_temperatura DECIMAL,
-	luminosidade DECIMAL,
-	lm35_temperatura DECIMAL,
-	chave TINYINT,
-	momento DATETIME,
-	fk_aquario INT,
-	FOREIGN KEY (fk_aquario) REFERENCES aquario(id)
-);
-
-insert into empresa (razao_social, codigo_ativacao) values ('Empresa 1', 'ED145B');
-insert into empresa (razao_social, codigo_ativacao) values ('Empresa 2', 'A1B2C3');
-insert into aquario (descricao, fk_empresa) values ('Aquário de Estrela-do-mar', 1);
-insert into aquario (descricao, fk_empresa) values ('Aquário de Peixe-dourado', 2);
